fedd13e59521d495b28f8250670bf9110e460246
Paweł Gawżyński <pwgawzynski@outlook.com>
2021-03-31 10:19:24 +0200

    MASTER

--- ALgorytm_Konika_Szachowego/main.c
+++ ALgorytm_Konika_Szachowego/main.c
@@ -1,13 +1,15 @@
 #include "initStructures.h"
 
 int main() {
-
 	int size_x, size_y;
 	int position_x, position_y, current=1;
 	pp_chessboard chessBoard;
 	chessBoard=getdata(&size_x, &size_y, &position_x, &position_y);
-	solve_knight_problem(chessBoard, &position_x, &position_y, &size_x, &size_y, &current);
-	printf("\n\n\n");
-	print_board(chessBoard, &size_x, &size_y);
+	if (solve_knight_problem(chessBoard, &position_x, &position_y, &size_x, &size_y, &current)) {
+		printf("\n\n\n");
+		print_board(chessBoard, &size_x, &size_y);
+	}
+	else printf("Inpossible");
+	
 	return 0;
 }
