66b4b4682ffca9bf651942b2ebbeb95dba6977c4
Paweł Gawżyński <pwgawzynski@outlook.com>
2021-03-31 08:14:55 +0200

    Debbuged

--- ALgorytm_Konika_Szachowego/main.c
+++ ALgorytm_Konika_Szachowego/main.c
@@ -6,7 +6,7 @@ int main() {
 	int position_x, position_y, current=1;
 	pp_chessboard chessBoard;
 	chessBoard=getdata(&size_x, &size_y, &position_x, &position_y);
-	solve_knight_problem(chessBoard, &position_x, &position_y, &size_x, &size_y, current);
+	solve_knight_problem(chessBoard, &position_x, &position_y, &size_x, &size_y, &current);
 	printf("\n\n\n");
 	print_board(chessBoard, &size_x, &size_y);
 	return 0;
