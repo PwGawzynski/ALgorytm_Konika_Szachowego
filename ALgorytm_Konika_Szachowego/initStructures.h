2d89f120310c0b65cbaa5f1cb86d281af44a01c3
Paweł Gawżyński <pwgawzynski@outlook.com>
2021-03-31 09:11:37 +0200

    brackests

--- ALgorytm_Konika_Szachowego/initStructures.h
+++ ALgorytm_Konika_Szachowego/initStructures.h
@@ -73,8 +73,8 @@ int moves[8][2] = {
 int valid(int* x, int* y, pp_chessboard chessBoard, int* size_x, int* size_y) // wybranie najbardziej optymalnego skoku bior¹c pod uwage co bedzie po tym skoku
 {
     return
-        (*x) >= 0 && (*x) < (*size_y)&& // sprawdza czy nie wychodzi poza szachownice 
-        (*y) >= 0 && (*y) < (*size_x)&& 
+       ( (*x) >= 0) && ((*x) < (*size_y))&& // sprawdza czy nie wychodzi poza szachownice 
+       ( (*y) >= 0 )&& ((*y) < (*size_x))&& 
         chessBoard[(*y)][(*x)] == 0; // sprawdza czy ta pozycja nie zostala wczesniej odwiedzona
 }
 
@@ -99,15 +99,15 @@ bool solve_knight_problem(pp_chessboard chessBoard, int* position_x, int* positi
 		valids[i][2] = (*position_y) + moves[i][1];
 		if (valid(&valids[i][1], &valids[i][2],chessBoard, (size_x), (size_y)))
 			for (j = 0; j < 8; j++)
-				if (valid(valids[i][1] + moves[j][0]? &valids[i][1]: &valids[i][1],
-					valids[i][2] + moves[j][1]? &valids[i][2]: &valids[i][2], chessBoard, (size_x), (size_y)))
+				if (valid((valids[i][1] + moves[j][0]? &valids[i][1]: &valids[i][1]),
+					(valids[i][2] + moves[j][1]? &valids[i][2]: &valids[i][2]), chessBoard, (size_x), (size_y)))
 					valids[i][0]++;
 	}
 
 	/* Sort and try each one. */
 	qsort(&valids[0][0], 8, 3 * sizeof(int), &compar);
 	for (i = 0; i < 8; i++)
-		if (valid(&valids[i][1], &valids[i][2], chessBoard, (size_x), (size_y)) && solve_knight_problem(chessBoard, (&valids[i][1]),(& valids[i][2]), (size_x), (size_y), (*current+=1)?current:0 ))
+		if (valid((&valids[i][1]), (&valids[i][2]), chessBoard, (size_x), (size_y)) && solve_knight_problem(chessBoard, (&valids[i][1]),(& valids[i][2]), (size_x), (size_y), (*current+=1)?current:0 ))
 			return 1;
 
 	/* Dead end: back off. */
