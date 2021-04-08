6443e722ecc11d88be9edfe65b2b72273f093034
Paweł Gawżyński <pwgawzynski@outlook.com>
2021-04-08 19:39:12 +0200

    0804

--- ALgorytm_Konika_Szachowego/main.c
+++ ALgorytm_Konika_Szachowego/main.c
@@ -1,6 +1,23 @@
-#include "initStructures.h"
+#include "status.h"
+#include "init_Structures.h"
+#include "solve_problem.h"
+#include "allegro_handler.h"
+#include "allegro_menu_start.h"
+
+
+// ____CONSTANTS____ //
+const float FPS = 30.0;
+unsigned int resolution_x = 1920;
+unsigned int resolution_y = 1080;
 
 int main() {
+	ALLEGRO_TIMER* timer = NULL;
+	ALLEGRO_EVENT_QUEUE* queue = NULL;
+	ALLEGRO_DISPLAY* display = NULL;
+	ALLEGRO_FONT* font = NULL;
+	ALLEGRO_BITMAP* background = NULL;
+	allegro_game_init(&timer, &display, &queue, &font, &background, &resolution_x, &resolution_y, &FPS);
+	init_menu(&timer, &display, &queue, &font, &background, &resolution_x, &resolution_y, &FPS);
 	int size_x, size_y;
 	int position_x, position_y, current=1;
 	pp_chessboard chessBoard;
@@ -10,6 +27,12 @@ int main() {
 		print_board(chessBoard, &size_x, &size_y);
 	}
 	else printf("Inpossible");
-	
+
+
+
+	al_destroy_font(font);
+	al_destroy_display(display);
+	al_destroy_timer(timer);
+	al_destroy_event_queue(queue);
 	return 0;
 }
